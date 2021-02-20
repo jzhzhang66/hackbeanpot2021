eaaaa235cce803899a2c8db3a7eb8ac32c730c21
jzhzhang66 <jzhzhang66@gmail.com>
2021-02-20 13:23:36 -0500

    finished analyzing sentiment

--- back-end/index.js
+++ back-end/index.js
@@ -1,4 +1,7 @@
 const {getPics} = require('./spotify')
 const {getBooksByTitle} = require('./googlebooks')
+const { analyzetone } = require('./analyzetone')
 
 //getPics()
+
+analyzetone('Today was such a great day. I ate a lot of cake')
