37d2c91201ec6821c046633870d181ad436440ef
jzhzhang66 <jzhzhang66@gmail.com>
2021-02-20 12:55:57 -0500

    updating branch

--- back-end/index.js
+++ back-end/index.js
@@ -8,3 +8,10 @@ const { analyzetone } = require('./analyzetone');
 
 
 analyzetone('Today I woke up and started crying because my dog died. But then, it came back to life so then I started smiling.')
+
+
+
+
+
+
+
