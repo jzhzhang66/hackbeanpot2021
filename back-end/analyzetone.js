b80c2498870bbe1f2b0792c33aa1b644bf56c6c0
jzhzhang66 <jzhzhang66@gmail.com>
2021-02-20 12:52:41 -0500

    analyze tone function finished

--- back-end/analyzetone.js
+++ back-end/analyzetone.js
@@ -6,6 +6,15 @@ const key = '55bSeiSLL-E_MhveNAmifQYZIhAkrkByvcs76e7qRKze'
 
 async function analyzetone(text) {
     let toneResults;
+    const tones = {
+        anger: 0,
+        fear: 0, 
+        joy: 0, 
+        sadness: 0, 
+        analytical: 0, 
+        confident: 0, 
+        tentative: 0
+    }
     const toneAnalyzer = new ToneAnalyzerV3({
         version: '2017-09-21',
         authenticator: new IamAuthenticator({
@@ -28,7 +37,45 @@ async function analyzetone(text) {
         });
 
     const sentenceTones = toneResults.result.sentences_tone
-    
+    for (var i = 0; i < sentenceTones.length; i++) {
+        const currTones = sentenceTones[i].tones
+        for (var j = 0; j < currTones.length; j++) {
+            const score = currTones[j].score 
+            switch(currTones[j].tone_id) {
+                case 'anger':
+                    tones.anger = tones.anger + score
+                    break;
+                case 'fear':
+                    tones.fear = tones.fear + score 
+                    break;
+                case 'joy':
+                    tones.joy = tones.joy + score
+                    break;
+                case 'sadness': 
+                    tones.sadness = tones.sadness + score
+                    break;
+                case 'analytical': 
+                    tones.analytical = tones.analytical + score
+                    break;
+                case 'confident':
+                    tones.confident = tones.confident + score
+                    break;
+                case 'tentative': 
+                    tones.tentative = tones.tentative + score 
+                    break;
+            }
+        }
+    }
+    const len = sentenceTones.length
+    tones.anger = tones.anger / len
+    tones.fear = tones.fear / len
+    tones.joy = tones.joy / len
+    tones.sadness = tones.sadness / len
+    tones.analytical = tones.analytical / len
+    tones.confident = tones.confident / len
+    tones.tentative = tones.tentative / len
+
+    return tones 
 }
 
 module.exports = {
