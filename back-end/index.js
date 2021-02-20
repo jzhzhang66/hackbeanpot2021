be419f3e5a667e55b32032cbc9b09adba905bb2f
jzhzhang66 <jzhzhang66@gmail.com>
2021-02-19 22:17:32 -0500

    starter code

--- back-end/index.js
+++ back-end/index.js
@@ -1,19 +1,3 @@
-var express = require('express');
-var app = express();
-var axios = require('axios');
+const {getPics} = require('./spotify')
 
-var port = process.env.PORT || 8080;
-
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers',
-        'Content-Type, X-Requested-With, Origin');
-    res.header('Access-Control-Allow-Methods',
-        'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-    next();
-});
-
-require("./people")(app)
-app.listen(port, function () {
-    console.log('Our app is running on http://localhost:' + port);
-  });
+getPics()
