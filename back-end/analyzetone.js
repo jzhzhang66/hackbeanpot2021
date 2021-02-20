3cb739bbc9f61623137c6455e1234fe419566ec9
jzhzhang66 <jzhzhang66@gmail.com>
2021-02-20 12:11:41 -0500

    added async await

--- back-end/analyzetone.js
+++ back-end/analyzetone.js
@@ -4,7 +4,8 @@ const { IamAuthenticator } = require('ibm-watson/auth');
 const url = 'https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/ea9c979b-20f3-4d57-8ead-cd95e900ef36'
 const key = '55bSeiSLL-E_MhveNAmifQYZIhAkrkByvcs76e7qRKze'
 
-function analyzetone(text) {
+async function analyzetone(text) {
+    let toneResults;
     const toneAnalyzer = new ToneAnalyzerV3({
         version: '2017-09-21',
         authenticator: new IamAuthenticator({
@@ -18,13 +19,15 @@ function analyzetone(text) {
         contentType: 'application/json',
       };
       
-      toneAnalyzer.tone(toneParams)
+      await toneAnalyzer.tone(toneParams)
         .then(toneAnalysis => {
-          console.log(JSON.stringify(toneAnalysis, null, 2));
+          toneResults = JSON.stringify(toneAnalysis, null, 2)
         })
         .catch(err => {
           console.log('error:', err);
-        });    
+        });
+
+    console.log(toneResults)
 }
 
 module.exports = {
