b938173c22950e84d7eb2b4144c80ac944b3ae12
chongmegan <chong.m@northeastern.edu>
2021-02-20 19:48:09 -0500

    styling artists page

--- front-end/book-toons/src/pages/artists.js
+++ front-end/book-toons/src/pages/artists.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './artists.css';
+import Header from '../components/header';
+import Title from '../components/title';
 
-function Artists() {
-    return (
-        <div>artists</div>
-    );
+class Artists extends React.Component {
+    render() {
+        return (
+            <>
+            <Header/>
+            <Title text="tell us your top 3 artists" width={'580px'}/>
+            <input className="artist-search" type="text"></input>
+            <input className="artist-search" type="text"></input>
+            <input className="artist-search" type="text"></input>
+            <div className="artist-buttoncontainer">
+                <button id="artist-continue">continue</button>
+            </div>
+            </>
+        );
+    }
 }
 
 export default Artists;
